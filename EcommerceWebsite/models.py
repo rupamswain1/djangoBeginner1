a975ddb5e5e559437e219a96f4a68d8f96e3ae8c
rupamswain1 <rupamswain1@gmail.com>
2020-11-15 23:46:13 +0530

    model added and home page setup

--- EcommerceWebsite/models.py
+++ EcommerceWebsite/models.py
@@ -7,4 +7,7 @@ class Product(models.Model):
     product_category=models.CharField(max_length=200)
     product_description=models.CharField(max_length=5000)
     product_price=models.IntegerField()
+    product_image=models.ImageField(upload_to='media',default="")
     
+    def __str__(self):
+        return self.product_name
--- djangoBeginner1/urls.py
+++ djangoBeginner1/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import urls
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('ecomm/',include('EcommerceWebsite.urls')),
     path('blog/',include('Blog.urls')),
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
