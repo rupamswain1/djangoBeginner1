3d45b02b9dfd7b3238843c04e0a0e068613b25ca
rupamswain1 <rupamswain1@gmail.com>
2020-11-18 01:14:00 +0530

    ecommHome updated to add id and number to localstorage using java script

--- EcommerceWebsite/models.py
+++ EcommerceWebsite/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-
+   
     product_name=models.CharField(max_length=500)
     product_category=models.CharField(max_length=200)
     product_description=models.CharField(max_length=5000)
