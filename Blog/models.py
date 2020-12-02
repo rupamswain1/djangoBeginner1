1ff7d2c753f36845ace8ef5aa7657c045294b088
rupamswain1 <rupamswain1@gmail.com>
2020-12-03 01:34:09 +0530

    Blogs model created and structured home page to dislay view

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    blog_id= models.AutoField(primary_key=True)
+    blog_heading=models.CharField(max_length=200)
+    blog_content=models.CharField(max_length=2000)
+    blog_subheading=models.CharField(max_length=100)
+    blog_subContent=models.CharField(max_length=1000)
+    blog_addedBy=models.CharField(max_length=50)
+    blog_pubDate=models.DateField(auto_now=True)
+    blog_image=models.ImageField(upload_to='media',default="")
+    def __str__(self):
+        return str(self.blog_id)+"_"+self.blog_heading
--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import BlogPost
 # Create your views here.
+
 def blogHome(request):
-    return render(request,'blogHome.html')
+    blogs=BlogPost.objects.all()
+    return render(request,'blogHome.html',{'blogs':blogs})
 
 def blogView(request):
     return render(request,'blogView.html')
